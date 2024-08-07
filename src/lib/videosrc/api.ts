125aa5ad3e26fe0fb4c001f1a63330df35e35c01
marArcz <mar19arcz@gmail.com>
2024-08-08 00:51:19 +0800

    changed url

--- src/lib/videosrc/api.ts
+++ src/lib/videosrc/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function getMovieStreamSrc(id:number) {
     try {
-        const res = await axios.get(`https://vidsrc.xyz/embed/movie?tmdb=${id}/`);
+        const res = await axios.get(`https://vidsrc.xyz/embed/movie/${id}`);
         console.log('vidsrc: ', res)
         return res.data;
     } catch (error) {
