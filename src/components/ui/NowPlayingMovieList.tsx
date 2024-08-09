6d68ddf0f6210f85c690b2f4ec730c7b4622dfca
marArcz <mar19arcz@gmail.com>
2024-08-09 22:01:19 +0800

    resized image to w300

--- src/components/ui/NowPlayingMovieList.tsx
+++ src/components/ui/NowPlayingMovieList.tsx
@@ -37,7 +37,7 @@ const NowPlayingMovieList = ({ isLoading, movies, activeMovieIndex, setActiveMov
                                     if (index === activeMovieIndex) {
                                         return (
                                             <div key={movie.id} className="flex justify-center items-center relative now-playing-card active h-[160px] overflow-clip group">
-                                                <img src={tmdbImage(movie.backdrop_path || '','w200')} className="  w-full h-full object-cover absolute left-0 z-10 opacity-100 bg-slate-300" alt="" />
+                                                <img src={tmdbImage(movie.backdrop_path || '','w300')} className="  w-full h-full object-cover absolute left-0 z-10 opacity-100 bg-slate-300" alt="" />
                                                 <div className="w-full h-full bg-gray-100 opacity-5 absolute left-0 z-30 flex justify-center items-center group-hover:opacity-30 transition-all" >
                                                 </div>
                                                 <Link to={`/watch/${movie.id}`} className="glass bg-white bg-opacity-60 hover:bg-opacity-80 transition-all shadow border border-white rounded-[10px] relative px-6 py-3 z-30">
@@ -51,7 +51,7 @@ const NowPlayingMovieList = ({ isLoading, movies, activeMovieIndex, setActiveMov
                                     }
                                     return (
                                         <button key={movie.id} tabIndex={index} onFocus={() => setActiveMovieIndex(index)} onMouseEnter={() => setActiveMovieIndex(index)} className="flex justify-center group items-center relative now-playing-card h-[160px] overflow-clip">
-                                            <img src={tmdbImage(movie.backdrop_path)} className="w-full h-full object-cover absolute left-0 z-10 opacity-70 group-hover:opacity-100 transition" alt="" />
+                                            <img src={tmdbImage(movie.backdrop_path,'w300')} className="w-full h-full object-cover absolute left-0 z-10 opacity-70 group-hover:opacity-100 transition" alt="" />
                                         </button>
                                     )
                                 })
