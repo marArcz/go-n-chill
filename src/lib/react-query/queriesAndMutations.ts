9891ac8404194542a2c6b659916e3beff60d353e
marArcz <mar19arcz@gmail.com>
2024-08-11 00:15:12 +0800

    added movie to queryKeys

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,32 +4,32 @@ import { getMovieById, getMovies } from "../tmdb/api"
 
 export const useGetNowPlaying = (page = 1, limit: number | null = null, language = 'en-US') => {
     return useQuery({
-        queryKey: [QUERY_KEYS.GET_NOW_PLAYING],
+        queryKey: [QUERY_KEYS.GET_NOW_PLAYING, page],
         queryFn: () => getMovies('now_playing', page, limit, language)
     })
 }
 export const useGetPopular = (page = 1, limit = null, language = 'en-US') => {
     return useQuery({
-        queryKey: [QUERY_KEYS.GET_POPULAR],
+        queryKey: [QUERY_KEYS.GET_POPULAR, page],
         queryFn: () => getMovies('popular', page, limit, language)
     })
 }
 export const useGetTopRated = (page = 1, limit = null, language = 'en-US') => {
     return useQuery({
-        queryKey: [QUERY_KEYS.GET_TOP_RATED],
+        queryKey: [QUERY_KEYS.GET_TOP_RATED, page],
         queryFn: () => getMovies('top_rated', page, limit, language)
     })
 }
 export const useGetUpcoming = (page = 1, limit = null, language = 'en-US') => {
     return useQuery({
-        queryKey: [QUERY_KEYS.GET_UPCOMING],
+        queryKey: [QUERY_KEYS.GET_UPCOMING, page],
         queryFn: () => getMovies('upcoming', page, limit, language)
     })
 }
 
 export const useGetMovieById = (id: number | null | string) => {
     return useQuery({
-        queryKey: [QUERY_KEYS.GET_MOVIE_BY_ID],
+        queryKey: [QUERY_KEYS.GET_MOVIE_BY_ID, id],
         queryFn: () => getMovieById(id),
         enabled: !!id
     })
